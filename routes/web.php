afee5e25947bafe5416ddd3daf3fce270323764b
Abril González <abril.satoritech@gmail.com>
2024-02-11 22:11:14 -0600

    controllers single action and injection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ReportUserController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +22,40 @@ Route::get('/', function () {
 });
 
 Route::get('test', TestController::class);
+
+Route::get('report-user/{userId}', ReportUserController::class);
+
+Route::prefix('users/')
+->name('users.')
+->group(function(){
+    Route::get('get-user/{user}',[UserController::class,'getUser'])
+    ->name('get_name');
+
+    Route::post('create/{user}',[UserController::class,'create'])
+    ->name('create');
+
+    Route::post('update/{user}',[UserController::class,'update'])
+    ->name('update');
+
+    Route::post('update-2/{todoId}/{user}',[UserController::class,'update2'])
+    ->name('update_2');
+
+    Route::get('get-user-by-email/{user:email}',[UserController::class,'getUserByEmail'])
+    ->name('get_user_by_email');
+
+    Route::get('get-user-todo/{user}/{todo}',[UserController::class,'getUserTodo'])
+    ->name('get_user_todo');
+
+    Route::get('get-user-todo-2/{user}/{todo:state_id}',[UserController::class,'getUserTodo'])
+    ->name('get_user_todo_2')
+    ->missing(function (){
+        return response()->json([
+            'text' => 'Uno de los elementos solicitados no se encuentra disponible'
+        ]);
+    });
+});
+
+
+Route::get('csrf', function(){
+    return csrf_token();
+})->name('csrf');
